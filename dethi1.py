f1f822daffb19e1ddd8aad0b27d97c8a6d5f5ae2
trongthanhqvo <thanhlio3625328@gmail.com>
2021-06-19 17:52:54 +0700

    update de thi 1

---
+++ dethi1.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy
+import numpy as np
+
+# 1
+I = cv2.imread("./anhthi/the_cancuoc_congdan.jpg")
+# cv2.imshow("I", I[:, :, 0])
+
+
+# 2
+# xử dụng hàm của opencv
+# Ig = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("Ig function opencv", Ig)
+
+    # cách 2
+Ig = np.zeros((I.shape[0], I.shape[1]), dtype="uint8")
+for i in range(0, Ig.shape[0]):
+    for j in range(0, Ig.shape[1]):
+        Ig[i][j] = int(0.39 * I[i][j][2] + 0.5 * I[i][j][1] + 0.11 * I[i][j][0])
+cv2.imshow("Ig convert", Ig)
+
+
+# 3
+# 0 là màu đen, 255 là màu trắng
+thresh, Ib = cv2.threshold(Ig, 0, 255, cv2.THRESH_OTSU)
+# cv2.imshow("Ib", Ib)
+
+# 4
+Im = cv2.blur(Ig, (5, 5))
+cv2.imshow("Im", Im)
+
+
+cv2.waitKey(0)
+
