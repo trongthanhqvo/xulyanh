d85e023d8f1a5dbb49ea8f1231e84b52747f4f54
trongthanhqvo <thanhlio3625328@gmail.com>
2021-06-20 02:01:16 +0700

    done de thi 1

--- dethi1.py
+++ dethi1.py
@@ -17,7 +17,7 @@ Ig = np.zeros((I.shape[0], I.shape[1]), dtype="uint8")
 for i in range(0, Ig.shape[0]):
     for j in range(0, Ig.shape[1]):
         Ig[i][j] = int(0.39 * I[i][j][2] + 0.5 * I[i][j][1] + 0.11 * I[i][j][0])
-cv2.imshow("Ig convert", Ig)
+# cv2.imshow("Ig convert", Ig)
 
 
 # 3
@@ -27,8 +27,13 @@ thresh, Ib = cv2.threshold(Ig, 0, 255, cv2.THRESH_OTSU)
 
 # 4
 Im = cv2.blur(Ig, (5, 5))
-cv2.imshow("Im", Im)
+# cv2.imshow("Im", Im)
 
+# 5
+contours, con = cv2.findContours(Im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(I, contours, -1, (255, 0, 0), 3)
+cv2.imshow("Contours I", I)
 
 cv2.waitKey(0)
 
